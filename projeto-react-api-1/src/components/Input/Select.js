e4292272f42905c751f60774000a185c40c3857c
juliaraujo10 <alvs.july@gmail.com>
2024-04-11 20:41:55 -0300

    inserção de dados

--- projeto-react-api-1/src/components/Input/Select.js
+++ projeto-react-api-1/src/components/Input/Select.js
@@ -1,31 +1,28 @@
 import style from '../Input/Select.modules.css'
 
-export default function select({
-  text,
-  name,
-  options,
-  handlerOnChange,
-  value,
-}) 
+export default function select({text, name, options, handlerOnChange, value}) 
 
 {
   return (
     <div className={style.form_control}>
       <label htmlFor={name}>{text}</label>
-        <select name={name} id={id}>
+        <select name={name} id={id} onChange={handlerOnChange}>
 
         <option>Selecione uma categoria</option>
 
         {
             options.map((option) => (
-                <option value={option.id} key={option.id}>
+                <option
+                  value={option.id}
+                  key={option.id}>
 
-                    option.name
+                    {option.name}
 
                 </option>
             ))
         }
         </select>
+        
     </div>
   )
 }
--- projeto-react-api-1/src/pages/NovoLivro/NovoLivro.js
+++ projeto-react-api-1/src/pages/NovoLivro/NovoLivro.js
@@ -1,14 +1,24 @@
-import {useSate, useEffect} from 'react';
+import {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom'
 
 import Input from '../../components/Input/Input'
 import Select from '../../components/Input/Select'
 
 import styles from '../../pages/NovoLivro/NovoLivro.module.css'
 
-export default function NovoLivro()
- {
-  const [categories, setCategories] = useSate([]);
+export default function NovoLivro(){
 
+  /*OBJETO DE NAVEGAÇÃO*/
+  const navigate = useNavigate();
+
+  /*STATE DE DADOS DAS CATEGORIAS VINDAS DO ARQUIVO db.json*/
+  const [categories, setCategories] = useState([]);
+
+  /*STATE DE DADPS QUE VAI ARMAZENAR O OBJETO JSON DE LIVRO*/
+  const [book, setBook] = useState({});
+
+
+  /**/
   useEffect(()=>{
   fetch (
       'http://localhost:5000/categories',
@@ -34,46 +44,102 @@ export default function NovoLivro()
 
       )
     })
+
+    /*HANDLER DE CAPTURA DOS DADOS DE INPUT*/
+
+      function handlerChangeBook(event) {
+          setBook({...book, [event.target.name] : event.target.value});
+          console.log(book)
+    }
+
+    /*HANDLER DE CAPTURA DOS DADOS DE SELECT*/
+
+      function handlerChangeCategory(event) {
+          setBook({...book, category:{
+            id:event.target.value,
+            category: event.target.options[event.target.selectedIndex.text]
+          }});
+          // console.log(book)
+    }
+
+    console.log(book)
+
+    /*INSERÇÃO DOS DADOS DE LIVRO*/
+    function creteBook(book) {
+
+        fetch('https://localhost:5000/books', {
+          method:'POST',
+          headers:{
+          'Content-Type':'application/json'
+          },
+          body: JSON.stringify(book)
+        })
+        .then(
+          (resp)=>resp.json()
+        )
+        .then(
+          (data)=>{
+            console.log(data);
+            navigate('/livros');
+          }
+        )
+        .catch(
+          (err)=>{ console.log(err)}
+        )
+    }
+    
+    /*FUNÇÃO DE SUBMIT*/
+    function submit(event) {
+      event.preventDefault();
+      creteBook(book)
+    }
+
   return (
     <section className={styles.novolivros_container}>
-      <h1>Cadastre livro</h1>
+      <h1>Cadastro de livro</h1>
 
-      <form>
+      <form onSubmit={submit}>
         
-        <Input
-          type="text"
-          name="nome_livro"
-          id="nome_livro"
-          placeholder="Digite o titulo do livro"
-          text="Digite o titulo do livro"
-        />
+          <Input
+            type="text"
+            name="nome_livro"
+            id="nome_livro"
+            placeholder="Digite o titulo do livro"
+            text="Digite o titulo do livro"
+            handlerOnChange={handlerChangeBook}
+          />
 
-        <Input
-          type="text"
-          name="nome_autor"
-          id="nome_autor"
-          placeholder="Digite o nome do autor"
-          text="Digite o nome do autor"
-        />
+          <Input
+            type="text"
+            name="nome_autor"
+            id="nome_autor"
+            placeholder="Digite o nome do autor"
+            text="Digite o nome do autor"
+            handlerOnChange={handlerChangeBook}
+          />
 
-        <Input
-          type="text"
-          name="descricao_livro"
-          id="descricao_livro"
-          placeholder="Digite a descricao do livro"
-          text="digite a descricao do livro"
-        />
+          <Input
+            type="text"
+            name="descricao_livro"
+            id="descricao_livro"
+            placeholder="Digite a descricao do livro"
+            text="digite a descricao do livro"
+            handlerOnChange={handlerChangeBook}
+          />
       
-      <Select 
-        name="categoria_id"
-        text="Selecione a categoria do livro"
-        options={categories}
-      />
+          <Select 
+            name="categoria_id"
+            text="Selecione a categoria do livro"
+            options={categories}
+            handlerOnChange={handlerChangeCategory}
+          />
 
-      <p>
-        <Input type='submit' value='Cadastrar um livro'/>
-      </p>
-    </form>
+        <p>
+          <Input type='submit' value='Cadastrar um livro'/>
+        </p>
+
+      </form>
+
     </section>
   )
 }
