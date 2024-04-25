662eb1ee988bf4c02d80c4d7014baccd1799eb34
juliaraujo10 <alvs.july@gmail.com>
2024-04-25 14:15:45 -0300

    message

--- projeto-react-api-1/src/pages/Livros/Livros.js
+++ projeto-react-api-1/src/pages/Livros/Livros.js
@@ -1,9 +1,25 @@
+import { useLocation } from 'react-router-dom'
+
 import styles from './Livro.module.css'
 
+import Message from '../../components/Message/Message'
+
 export default function Livros() {
+  const location = useLocation();
+  let message='';
+  console.log(location.state);
+  if(location.state){
+    message = location.state
+  }
+
   return (
     <section className={styles.livros_container}>
-      <h1>Aqui vai ser listado seus livros</h1>
+      <div>
+        <h3>Livros</h3>
+        {
+          message && <Message msg={message} type="success"/>
+        }
+      </div>
     </section>
   )
 }
--- projeto-react-api-1/src/pages/NovoLivro/NovoLivro.js
+++ projeto-react-api-1/src/pages/NovoLivro/NovoLivro.js
@@ -1,145 +1,119 @@
-import {useState, useEffect} from 'react';
-import {useNavigate} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import Input from '../../components/Input/Input'
-import Select from '../../components/Input/Select'
+import Select from '../../components/Select/Select'
 
-import styles from '../../pages/NovoLivro/NovoLivro.module.css'
+import styles from './NovoLivro.module.css'
 
-export default function NovoLivro(){
-
-  /*OBJETO DE NAVEGAÇÃO*/
+export default function NovoLivro() {
   const navigate = useNavigate();
 
-  /*STATE DE DADOS DAS CATEGORIAS VINDAS DO ARQUIVO db.json*/
-  const [categories, setCategories] = useState([]);
-
-  /*STATE DE DADPS QUE VAI ARMAZENAR O OBJETO JSON DE LIVRO*/
-  const [book, setBook] = useState({});
 
 
-  /**/
-  useEffect(()=>{
-  fetch (
-      'http://localhost:5000/categories',
-      {
-        
-        method:'GET',
-        headers:{
-          'Content-Type':'application/json'
-        }
-      }).then(
-        (resp)=> 
-          resp.json()
-      ).then(
-        (data)=> 
-        {
-          setCategories(data);
-          console.log(data);
-        }
-      ).catch(
-        (error)=>{
-          console.log(error);
-        }
+  const [categories, setCategories] = useState([])
+  const [book, setBook] = useState({})
 
-      )
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'aplication/json',
+      },
     })
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data)
+        console.log(data)
+      })
 
-    /*HANDLER DE CAPTURA DOS DADOS DE INPUT*/
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
 
-      function handlerChangeBook(event) {
-          setBook({...book, [event.target.name] : event.target.value});
-          console.log(book)
-    }
-
-    /*HANDLER DE CAPTURA DOS DADOS DE SELECT*/
-
-      function handlerChangeCategory(event) {
-          setBook({...book, category:{
-            id:event.target.value,
-            category: event.target.options[event.target.selectedIndex.text]
-          }});
-          // console.log(book)
-    }
-
+  function handleChangeBook(event) {
+    setBook({ ...book, [event.target.name]: event.target.value })
     console.log(book)
+  }
 
-    /*INSERÇÃO DOS DADOS DE LIVRO*/
-    function creteBook(book) {
+  function handleChangeCategory(event) {
+    setBook({
+      ...book,
+      categories: {
+        id: event.target.value,
+        categories: event.target.options[event.target.selectedIndex].text,
+      },
+    })
+    console.log(book)
+  }
 
-        fetch('https://localhost:5000/books', {
-          method:'POST',
-          headers:{
-          'Content-Type':'application/json'
-          },
-          body: JSON.stringify(book)
+    function createBook(book) {
+      fetch('http://localhost:5000/books', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'aplication/json',
+        },
+        body: JSON.stringify(book),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+          navigate('/livros', {state: 'lixo'})
         })
-        .then(
-          (resp)=>resp.json()
-        )
-        .then(
-          (data)=>{
-            console.log(data);
-            navigate('/livros');
-          }
-        )
-        .catch(
-          (err)=>{ console.log(err)}
-        )
-    }
-    
-    /*FUNÇÃO DE SUBMIT*/
-    function submit(event) {
-      event.preventDefault();
-      creteBook(book)
+        .catch((error) => {
+          console.log(error)
+        })
     }
 
+    function submit(event) {
+      event.preventDefault()
+      createBook(book)
+      
+    }
   return (
     <section className={styles.novolivros_container}>
-      <h1>Cadastro de livro</h1>
+      <h1>Cadastre livro</h1>
 
       <form onSubmit={submit}>
-        
-          <Input
-            type="text"
-            name="nome_livro"
-            id="nome_livro"
-            placeholder="Digite o titulo do livro"
-            text="Digite o titulo do livro"
-            handlerOnChange={handlerChangeBook}
-          />
+        <Input
+          type="text"
+          name="nome_livro"
+          id="nome_livro"
+          placeholder="Digite o titulo do livro"
+          text="Digite o titulo do livro"
+          handlerOnChange={handleChangeBook}
+        />
 
-          <Input
-            type="text"
-            name="nome_autor"
-            id="nome_autor"
-            placeholder="Digite o nome do autor"
-            text="Digite o nome do autor"
-            handlerOnChange={handlerChangeBook}
-          />
+        <Input
+          type="text"
+          name="nome_autor"
+          id="nome_autor"
+          placeholder="Digite o nome do autor"
+          text="Digite o nome do autor"
+          handlerOnChange={handleChangeBook}
+        />
 
-          <Input
-            type="text"
-            name="descricao_livro"
-            id="descricao_livro"
-            placeholder="Digite a descricao do livro"
-            text="digite a descricao do livro"
-            handlerOnChange={handlerChangeBook}
-          />
-      
-          <Select 
-            name="categoria_id"
-            text="Selecione a categoria do livro"
-            options={categories}
-            handlerOnChange={handlerChangeCategory}
-          />
+        <Input
+          type="text"
+          name="descricao_livro"
+          id="descricao_livro"
+          placeholder="Digite a descricao do livro"
+          text="digite a descricao do livro"
+          handlerOnChange={handleChangeBook}
+        />
 
+        <Select
+          name="categoria-id"
+          text="selecione a categoria do livro"
+          options={categories}
+          handlerOnChange={handleChangeCategory}
+        />
         <p>
-          <Input type='submit' value='Cadastrar um livro'/>
+          <button type="submit">Enviar</button>
         </p>
-
       </form>
-
     </section>
   )
 }
