fd2e2eeab8e7d72e9f65bd8de13f566b3acaea9b
juliaraujo10 <alvs.july@gmail.com>
2024-05-16 20:59:29 -0300

    processo de exclusão

--- projeto-react-api-1/src/pages/Livros/Livros.js
+++ projeto-react-api-1/src/pages/Livros/Livros.js
@@ -11,18 +11,41 @@ function Livros() {
 
   const [books, setBooks] = useState([]);
 
+  /*  ESTADO DE DADOS DA MENSAGEM DE EXCLUSÃO DE LIVRO */
+  const [bookMessage, setBookMessage] = useState('');
+
   useEffect(()=>{
       fetch('http://localhost:5000/books', {
         method: 'GET',
         headers: {
           'Content-Type':'application/json'
-        }
+        },
       })
       .then((resp)=>resp.json())
       .then((data)=>setBooks(data))
       .catch((err)=>{console.log(err)});
 
-  })
+  }, [books])
+
+  /*FUNÇÃO DE EXCLUSAO DE LIVRO*/
+  function removeBooks(id) {
+
+    fetch(`http://localhost:5000/books/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type':'application/json'
+      },
+    })
+    .then(resp=> resp.json())
+    .then(
+      (data)=>{
+        // setBooks(books.filter((book_data)=>book_data.id != id))
+        // alert('LIVRO EXCLUÍDO')
+        setBookMessage('LIVRO EXCLUÍDO COM SUCESSO!');
+      }
+    )
+    .catch(err=>console.log(err))
+  }
 
   const location = useLocation();
   let message = '';
@@ -36,6 +59,8 @@ function Livros() {
     <section className={styles.livros_container}>
       <h1>Aqui serão listados seus<span>livros</span></h1>
 
+      {/*MENSAGEM DE SUCESSO PARA CADASTRO*/}
+
       {
         message && (
             <Message
@@ -45,6 +70,16 @@ function Livros() {
         )
       }
 
+      {/*MENSAGEM DE SUCESSO PARA EXCLUSÃO*/}
+
+      {
+        bookMessage && (
+            <Message
+                msg={bookMessage}
+                type='sucess'
+            />
+        )
+      }
       {/* <Container> */}
         {/* <CardBook
           id='1'
@@ -74,7 +109,9 @@ function Livros() {
               id={book.id}
               livro={book.nome_livro}
               autor={book.nome_autor}
-              categoria={book.category.category}>
+              categoria={book.category.category}
+              key={book.id}
+              handlerRemove={removeBooks}>
           </CardBook>
         ))
       }
